8a5da22f2d370e79518894ae9fb7a5a63bda11bb
Nex-opensourcehorray <nex-opensourcehorray@users.noreply.github.com>
2024-10-07 01:21:08 +0800

    do some modification base on the requirement.

--- Prac04/subject_reader.py
+++ Prac04/subject_reader.py
@@ -7,13 +7,15 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    """The main function that running two functions."""
+    load_data()
+    get_lecture_detailed()
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    Celebrity = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,6 +25,20 @@ def load_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        Celebrity.append(parts)
+        print(Celebrity)
+    input_file.close()
+
+
+def get_lecture_detailed():
+    """Read data from file formatted and print the subject,lecturer,number of students in detailed."""
+    input_file = open(FILENAME)
+    for line in input_file:
+        # To change every line with strip and split.
+        line = line.strip()
+        Parts = line.split(',')
+        print(f"{Parts[0]} is taught by {Parts[1]} and has {Parts[2]} students.")
+    print("----------")
     input_file.close()
 
 
