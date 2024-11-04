61327cea9c27072db4796e0990963fea27bcfca0
Nex-opensourcehorray <nex-opensourcehorray@users.noreply.github.com>
2024-11-04 21:05:34 +0800

    Complete the sorting and adding the user input and file restoration function as well.

--- Prac07/myguitars.py
+++ Prac07/myguitars.py
@@ -6,8 +6,8 @@ from Prac07.guitar import Guitar
 def main():
     """The actual real test about the guitar input."""
     guitars = []
+    get_user_guitar(guitars)
     in_file = open('guitars.csv', 'r')
-    in_file.readline()
     for line in in_file:
         names = line.strip().split(',')
         name = names[0]
@@ -17,6 +17,19 @@ def main():
         guitars.append(guitar_name)
     in_file.close()
     display_my_guitar(guitars)
+    write_back_csv(guitars)
+
+
+def get_user_guitar(guitars):
+    name = input("Name:")
+    while name != "":
+        """while name is not blank, if will continue grab the information from user."""
+        year = int(input("Year:"))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "Added to the file")
+        name = input("Name:")
 
 
 def display_my_guitar(guitars):
@@ -26,16 +39,23 @@ def display_my_guitar(guitars):
     if guitars:
         guitars.sort()
         print("My guitars:")
-        for i, guitar in enumerate(guitars, 1):
+        for i, guitar in enumerate(guitars, 0):
             if guitar.is_vintage():
                 vintage_string = " (vintage)"
             else:
                 vintage_string = ""
-            print(f"Guitar {i}: {guitar.name:{guitar_length}} ({guitar.year}), worth $ {guitar.cost:{cost_length}.2f}"
-                  f"{vintage_string}")
+            print(
+                f"Guitar {i + 1}: {guitar.name:{guitar_length}} ({guitar.year}), worth $ {guitar.cost:{cost_length}.2f}"
+                f"{vintage_string}")
 
     else:
         print("Can you please go and get one, very cool!")
+
+
+def write_back_csv(guitars):
+    with open('guitars.csv', 'w', encoding='utf-8') as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 
 if __name__ == '__main__':
