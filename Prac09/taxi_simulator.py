4aefc55ac076012da004302d3beea19af0870fd9
Nex-opensourcehorray <nex-opensourcehorray@users.noreply.github.com>
2024-11-18 21:59:41 +0800

    Complete the taxi simulator with adding a choice taxi option, display taxi's information, and drive taxi function, as well as bring in docstring.

--- Prac09/taxi_simulator.py
+++ Prac09/taxi_simulator.py
@@ -11,18 +11,21 @@ MENU = "q)uit, c)hoose taxi, d)rive\n>>>"
 
 
 def main():
+    """The main part of program to control."""
     total_spend = 0.0
-    your_taxi_list = None
+    your_taxi = None
     print("Let's drive!")
     choice = input(MENU).lower()
     while choice != "q":
         if choice == "c":
-            print("choose the taxi!")
+            your_taxi = choose_your_taxi()
         elif choice == "d":
-            if your_taxi_list is None:
+            if your_taxi is None:
                 print("You need to choose a taxi before you can drive!")
             else:
-                print("drive the Taxi!")
+                current_spend = drive_taxi(your_taxi)
+                total_spend += current_spend
+                print(f"Your {your_taxi.name} cost you ${current_spend:.2f}")
         else:
             print("Invalid choice!")
         print(f"Bill to date:{total_spend:.2f}")
@@ -30,6 +33,33 @@ def main():
 
     print(f"Total trip cost: ${total_spend:.2f}")
     print("Taxis are now:")
+    display_all_taxi()
+
+
+def choose_your_taxi():
+    """Choose a taxi to service you."""
+    print("Taxis available:")
+    display_all_taxi()
+    choose = int(input("Choose taxi:"))
+    if 0 <= choose < len(TAXIS):
+        return TAXIS[choose]
+    else:
+        print("Invalid Taxi Choose!")
+        return None
+
+
+def display_all_taxi():
+    """Print all the taxi from the list."""
+    for index_number, taxi in enumerate(TAXIS):
+        print(f"{index_number} - {taxi}")
+
+
+def drive_taxi(your_taxi):
+    """Control your Taxi with your requirement distance."""
+    distance = int(input("Drive how far?"))
+    your_taxi.drive(distance)
+    your_taxi.get_fare()
+    return your_taxi.get_fare()
 
 
 if __name__ == '__main__':
